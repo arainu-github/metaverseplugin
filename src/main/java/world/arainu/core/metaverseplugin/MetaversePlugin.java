49dedbf29a6da145e3e1f3c1d10755a9f9cbf2b2
kumitatepazuru <riku.ueda@outlook.jp>
2021-08-25 18:02:15 +0900

    Initial commit

---
+++ src/main/java/world/arainu/core/metaverseplugin/MetaversePlugin.java
@@ -0,0 +1,17 @@
+package world.arainu.core.metaverseplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MetaversePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
