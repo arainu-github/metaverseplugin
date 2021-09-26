333460d7d10fc3e8e5b75f3414aeeabb437b8805
kumitatepazuru <riku.ueda@outlook.jp>
2021-09-27 05:47:45 +0900

    updateを関数化

--- src/main/java/world/arainu/core/metaverseplugin/scheduler/LateScheduler.java
+++ src/main/java/world/arainu/core/metaverseplugin/scheduler/LateScheduler.java
@@ -18,52 +18,60 @@ public class LateScheduler extends BukkitRunnable {
     @Override
     public void run() {
         if(Bukkit.getWorlds().get(0).getTime() < old_time) {
-            List<Integer> mason_villager_use = Arrays.asList(0, 0, 0, 0, 0, 0, 0);
-            for (UUID uuid : Objects.requireNonNull(sqlUtil.getInstance().getuuidsbytype("mason-villager"))) {
-                Villager villager = (Villager) Bukkit.getEntity(uuid);
-                List<MerchantRecipe> recipes = Objects.requireNonNull(villager).getRecipes();
-                for (int i = 0; i < recipes.size(); i++) {
-                    mason_villager_use.set(i, mason_villager_use.get(i) + recipes.get(i).getUses());
-                }
-            }
-
-            HashMap<String, List<Integer>> quantity_t_minus2 = quantity_t_minus1;
-            quantity_t_minus1 = new HashMap<>();
-            quantity_t_minus1.put("mason-villager", mason_villager_use);
-
-            if (quantity_t_minus2.get("mason-villager") != null) {
-                List<Integer> mason_villager_p = quantity_p.get("mason-villager");
-                int minus2;
-                int minus1;
-                for (int i = 0; i < quantity_t_minus1.get("mason-villager").size(); i++) {
-                    minus2 = quantity_t_minus2.get("mason-villager").get(i);
-                    minus1 = quantity_t_minus1.get("mason-villager").get(i);
-                    if (0 < minus2 - minus1) {
-                        mason_villager_p.set(i, mason_villager_p.get(i) - 1);
-                    } else if (minus2 - minus1 < 0) {
-                        mason_villager_p.set(i, mason_villager_p.get(i) + 1);
-                    }
-                    Bukkit.getLogger().info("mason_villager_p:" + mason_villager_p.get(i));
-                }
-                quantity_p.replace("mason-villager", mason_villager_p);
-
-                List<MerchantRecipe> recipes = new ArrayList<>();
-                recipes.add(CommandSpawn.createRecipe(1, (int) Math.round(50 + (10 * Math.tanh(mason_villager_p.get(0) / 5f))), new ItemStack(Material.COAL)));
-                recipes.add(CommandSpawn.createRecipe(5, (int) Math.round(40 + (10 * Math.tanh(mason_villager_p.get(1) / 5f))), new ItemStack(Material.RAW_COPPER)));
-                recipes.add(CommandSpawn.createRecipe(5, (int) Math.round(40 + (10 * Math.tanh(mason_villager_p.get(2) / 5f))), new ItemStack(Material.RAW_IRON)));
-                recipes.add(CommandSpawn.createRecipe(10, (int) Math.round(40 + (10 * Math.tanh(mason_villager_p.get(3) / 5f))), new ItemStack(Material.RAW_GOLD)));
-                recipes.add(CommandSpawn.createRecipe(5, (int) Math.round(20 + (10 * Math.tanh(mason_villager_p.get(4) / 5f))), new ItemStack(Material.REDSTONE)));
-                recipes.add(CommandSpawn.createRecipe(50, (int) Math.round(20 + (6 * Math.tanh(mason_villager_p.get(5) / 5f))), new ItemStack(Material.DIAMOND)));
-                recipes.add(CommandSpawn.createRecipe(5, (int) Math.round(30 + (10 * Math.tanh(mason_villager_p.get(6) / 5f))), new ItemStack(Material.LAPIS_LAZULI)));
-
-                for (UUID uuid : Objects.requireNonNull(sqlUtil.getInstance().getuuidsbytype("mason-villager"))) {
-                    Villager villager = (Villager) Bukkit.getEntity(uuid);
-                    Objects.requireNonNull(villager).setRecipes(recipes);
-                }
-                Bukkit.getLogger().info("すべてのmason-villagerの価格を変更しました。");
-            }
+            update("mason-villager",Arrays.asList(
+                    Arrays.asList(1,50,10,Material.COAL),
+                    Arrays.asList(5,40,10,Material.RAW_COPPER),
+                    Arrays.asList(5,40,10,Material.RAW_IRON),
+                    Arrays.asList(10,40,10,Material.RAW_GOLD),
+                    Arrays.asList(5,20,10,Material.REDSTONE),
+                    Arrays.asList(50,20,6,Material.DIAMOND),
+                    Arrays.asList(5,30,10,Material.LAPIS_LAZULI)
+            ));
         }
         old_time = Bukkit.getWorlds().get(0).getTime();
+    }
+
+    private void update(String type, List<List<Object>> recipeData){
+        List<Integer> villager_use = Arrays.asList(0, 0, 0, 0, 0, 0, 0);
+        for (UUID uuid : Objects.requireNonNull(sqlUtil.getInstance().getuuidsbytype(type))) {
+            Villager villager = (Villager) Bukkit.getEntity(uuid);
+            List<MerchantRecipe> recipes = Objects.requireNonNull(villager).getRecipes();
+            for (int i = 0; i < recipes.size(); i++) {
+                villager_use.set(i, villager_use.get(i) + recipes.get(i).getUses());
+            }
+        }
+
+        HashMap<String, List<Integer>> quantity_t_minus2 = quantity_t_minus1;
+        quantity_t_minus1 = new HashMap<>();
+        quantity_t_minus1.put(type, villager_use);
+
+        if (quantity_t_minus2.get(type) != null) {
+            List<Integer> p = quantity_p.get(type);
+            int minus2;
+            int minus1;
+            for (int i = 0; i < quantity_t_minus1.get(type).size(); i++) {
+                minus2 = quantity_t_minus2.get(type).get(i);
+                minus1 = quantity_t_minus1.get(type).get(i);
+                if (0 < minus2 - minus1) {
+                    p.set(i, p.get(i) - 1);
+                } else if (minus2 - minus1 < 0) {
+                    p.set(i, p.get(i) + 1);
+                }
+                Bukkit.getLogger().info(type+"_p:" + p.get(i));
+            }
+            quantity_p.replace(type, p);
+
+            List<MerchantRecipe> recipes = new ArrayList<>();
+            for (List<Object> data: recipeData){
+                recipes.add(CommandSpawn.createRecipe((int) data.get(0),(int) Math.round((int) data.get(1) + ((int) data.get(2) * Math.tanh(p.get(0) / 5f))),new ItemStack((Material) data.get(3))));
+            }
+
+            for (UUID uuid : Objects.requireNonNull(sqlUtil.getInstance().getuuidsbytype(type))) {
+                Villager villager = (Villager) Bukkit.getEntity(uuid);
+                Objects.requireNonNull(villager).setRecipes(recipes);
+            }
+            Bukkit.getLogger().info("すべての"+type+"の価格を変更しました。");
+        }
     }
 
     private HashMap<String,List<Integer>> quantity_t_minus1 = new HashMap<>();
