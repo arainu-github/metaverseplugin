5ce5fcc7b41cce4b12f3a4023afee22cead8fd3a
Riku Ueda <riku.ueda@outlook.jp>
2021-10-05 19:42:57 +0900

    discordのやつ

--- src/main/java/world/arainu/core/metaverseplugin/listener/ServerListener.java
+++ src/main/java/world/arainu/core/metaverseplugin/listener/ServerListener.java
@@ -2,11 +2,18 @@ package world.arainu.core.metaverseplugin.listener;
 
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import github.scarsz.discordsrv.dependencies.jda.api.EmbedBuilder;
+import github.scarsz.discordsrv.dependencies.jda.api.JDA;
+import github.scarsz.discordsrv.util.DiscordUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import world.arainu.core.metaverseplugin.MetaversePlugin;
+
+import java.util.Objects;
 
 /**
  * サーバーの動作に関するイベントリスナーのクラス
@@ -25,5 +32,23 @@ public class ServerListener implements Listener {
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
         out.writeUTF("GetServer");
         player.sendPluginMessage(MetaversePlugin.getInstance(), "BungeeCord", out.toByteArray());
+    }
+
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent e) {
+        Bukkit.getLogger().warning(String.valueOf(Bukkit.hasWhitelist()));
+        if(Bukkit.hasWhitelist()){
+            if(!e.getPlayer().isWhitelisted()){
+                JDA jda = DiscordUtil.getJda();
+                Objects.requireNonNull(jda.getTextChannelById(MetaversePlugin.getConfiguration().getLong("discord.warn_channel"))).sendMessage(
+                    new EmbedBuilder()
+                            .setTitle("ホワイトリストに登録されていないプレイヤーがログインしようとしました")
+                            .addField("プレイヤー名",e.getPlayer().getName(),false)
+                            .addField("UUID", String.valueOf(e.getPlayer().getUniqueId()),false)
+                            .setThumbnail("https://crafatar.com/avatars/"+e.getPlayer().getUniqueId())
+                            .build()
+                ).queue();
+            }
+        }
     }
 }
