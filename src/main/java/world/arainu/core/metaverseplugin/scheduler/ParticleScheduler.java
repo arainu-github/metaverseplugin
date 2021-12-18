fc5f9e659d59f4725cd6cac2cd3ed556c9c5d50f
Riku Ueda <riku.ueda@outlook.jp>
2021-12-18 12:20:55 +0900

    ３次元Lineを追加

--- src/main/java/world/arainu/core/metaverseplugin/scheduler/ParticleScheduler.java
+++ src/main/java/world/arainu/core/metaverseplugin/scheduler/ParticleScheduler.java
@@ -12,13 +12,33 @@ public class ParticleScheduler extends BukkitRunnable {
 
     @Override
     public void run() {
-        for (ParticleUtil k: queue){
-            for (ParticleUtil.Vector i: k.getLines()) {
+
+        for (ParticleUtil k : queue) {
+            for (ParticleUtil.Vector i : k.getLines()) {
                 final double X = i.x2() - i.x1();
                 final double Y = i.y2() - i.y1();
                 final double S = Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2));
                 for (double j = 0; j < S; j += 1) {
-                    i.world().spawnParticle(Particle.END_ROD, i.show_player(), null, i.x1() + j * (X/S), 64, i.y1() + j * (Y/S), 10, 0.1, 5, 0.1, 0, null, true);
+                    i.world().spawnParticle(Particle.END_ROD, i.show_player(), null, i.x1() + j * (X / S), 64, i.y1() + j * (Y / S), 10, 0.1, 5, 0.1, 0, null, true);
+                }
+            }
+
+            for (ParticleUtil.Vector3D i : k.getThinLines()) {
+                final double X = i.x2() - i.x1();
+                final double Z = i.y2() - i.y1();
+                final double Y = i.z2() - i.z1();
+                final double r = Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2) + Math.pow(Z, 2));
+                final double θ = Math.acos(Z / r);
+                double ϕ = Math.signum(Y) * Math.acos(X / Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2)));
+                if (Double.isNaN(ϕ)) {
+                    ϕ = 0;
+                }
+                for (double j = 0; j < r; j += 0.5) {
+                    final double x = i.x1() + j * Math.sin(θ) * Math.cos(ϕ);
+                    final double z = i.z1() + j * Math.sin(θ) * Math.sin(ϕ);
+                    final double y = i.y1() + j * Math.cos(θ);
+//                    Bukkit.getLogger().info("x:"+x+",y:"+y+",z:"+z);
+                    i.world().spawnParticle(Particle.END_ROD, i.show_player(), null, x, y, z, 1, 0, 0, 0, 0, null, true);
                 }
             }
         }
--- src/main/java/world/arainu/core/metaverseplugin/utils/ParticleUtil.java
+++ src/main/java/world/arainu/core/metaverseplugin/utils/ParticleUtil.java
@@ -10,11 +10,19 @@ import java.util.List;
 public class ParticleUtil {
     @Getter
     private final ArrayList<Vector> lines = new ArrayList<>();
+    @Getter
+    private final ArrayList<Vector3D> thinLines = new ArrayList<>();
 
     public record Vector(double x1, double y1, double x2, double y2, World world, List<Player> show_player) {
+    }
+    public record Vector3D(double x1, double y1, double z1, double x2, double y2, double z2, World world, List<Player> show_player) {
     }
 
     public void addLine(Vector vector){
         lines.add(vector);
+    }
+
+    public void addThinLine(Vector3D vector){
+        thinLines.add(vector);
     }
 }
