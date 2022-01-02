74c47ccaa7acc9499a4cc362fb7092729d18b339
RIku Ueda <riku.ueda@outlook.jp>
2022-01-02 19:36:08 +0000

    説明文追加

--- src/main/java/world/arainu/core/metaverseplugin/listener/ServerListener.java
+++ src/main/java/world/arainu/core/metaverseplugin/listener/ServerListener.java
@@ -12,13 +12,16 @@ import me.leoko.advancedban.manager.TimeManager;
 import me.leoko.advancedban.utils.Punishment;
 import me.leoko.advancedban.utils.PunishmentType;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import world.arainu.core.metaverseplugin.MetaversePlugin;
 import world.arainu.core.metaverseplugin.store.ServerStore;
@@ -116,6 +119,12 @@ public class ServerListener implements Listener {
             markerData.remove(e.getPlayer());
             ServerStore.setMarkerData(markerData);
         }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        final String url = "https://data.arainu.world/advancements/?uuid="+e.getPlayer().getUniqueId();
+        e.getPlayer().sendMessage(Component.text("以下のサイトからあなたの進捗の達成度を確認できるようになりました！\n").append(Component.text(url).clickEvent(ClickEvent.openUrl(url)).decorate(TextDecoration.UNDERLINED)));
     }
 
     private final HashMap<UUID, TimeAndMsg> last_log_map = new HashMap<>();
