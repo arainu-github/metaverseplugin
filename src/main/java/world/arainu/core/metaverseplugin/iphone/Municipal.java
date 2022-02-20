a3a4cd699a365e71ee26fc834aa1d930cf7afeb7
Riku Ueda <riku.ueda@outlook.jp>
2022-02-20 10:55:55 +0900

    自治体システムの強化 既知のバグ：複数の自治体の住民になることができてしまう

--- src/main/java/world/arainu/core/metaverseplugin/iphone/Municipal.java
+++ src/main/java/world/arainu/core/metaverseplugin/iphone/Municipal.java
@@ -23,6 +23,7 @@ import world.arainu.core.metaverseplugin.utils.ChatUtil;
 import world.arainu.core.metaverseplugin.utils.ItemUtil;
 import world.arainu.core.metaverseplugin.utils.sqlUtil;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -36,20 +37,20 @@ import java.util.stream.Collectors;
 public class Municipal extends iPhoneBase {
     @Override
     public void executeGui(MenuItem menuItem) {
-        if(DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(menuItem.getClicker().getUniqueId()) != null) {
+        if (DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(menuItem.getClicker().getUniqueId()) != null) {
             final DynmapAPI dynmap = MetaversePlugin.getDynmap();
             final MarkerAPI marker = dynmap.getMarkerAPI();
             MarkerSet markerSet = marker.getMarkerSet("municipal");
-            if(markerSet == null) {
-                markerSet = marker.createMarkerSet("municipal","自治体",null,true);
+            if (markerSet == null) {
+                markerSet = marker.createMarkerSet("municipal", "自治体", null, true);
             }
-            List<MenuItem> menuList = markerSet.getAreaMarkers().stream().map(n -> Arrays.asList(n.getMarkerID(),n.getLabel())).map(
+            List<MenuItem> menuList = markerSet.getAreaMarkers().stream().map(n -> Arrays.asList(n.getMarkerID(), n.getLabel())).map(
                     data -> {
                         ItemStack item = new ItemStack(Material.PLAYER_HEAD);
                         SkullMeta skull = (SkullMeta) item.getItemMeta();
                         skull.setOwningPlayer(Bukkit.getOfflinePlayer(Objects.requireNonNull(sqlUtil.getMunicipal(data.get(0))).uuid()));
                         item.setItemMeta(skull);
-                        return new MenuItem(data.get(1),null,true, item);
+                        return new MenuItem(data.get(1), this::manage, true, item,data);
                     }
             ).collect(Collectors.toList());
             ItemStack item = new ItemStack(Material.SLIME_BALL);
@@ -57,8 +58,59 @@ public class Municipal extends iPhoneBase {
             Gui.getInstance().openMultiPageMenu(menuItem.getClicker(), "自治体メニュー", menuList,
                     new MenuItem("自分で自治体を作る", this::createMunicipal, true, item));
         } else {
-            ChatUtil.error(menuItem.getClicker(),"自治体機能はdiscordとminecraftを連携することによって使用できます。\niphone内の「discordと連携する」から、discordとminecraftを連携してください。");
+            ChatUtil.error(menuItem.getClicker(), "自治体機能はdiscordとminecraftを連携することによって使用できます。\niphone内の「discordと連携する」から、discordとminecraftを連携してください。");
         }
+    }
+
+    private void manage(MenuItem menuItem) {
+        List<?> data = (List<?>) menuItem.getCustomData();
+        ItemStack item = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta skull = (SkullMeta) item.getItemMeta();
+        skull.setOwningPlayer(Bukkit.getOfflinePlayer(Objects.requireNonNull(sqlUtil.getMunicipal((String) data.get(0))).uuid()));
+        item.setItemMeta(skull);
+        List<MenuItem> menus = new ArrayList<>();
+        if(menuItem.getClicker().isOp()){
+            ItemStack removeItem = new ItemStack(Material.BARRIER);
+            removeItem.lore(List.of(Component.text("Moderator only")));
+            menus.add(new MenuItem("自治体を削除する",this::remove,true,removeItem,data));
+        }
+        menus.add(new MenuItem("この自治体の住民になる",this::addResidents,true,Material.NAME_TAG,data.get(0)));
+        menus.add(new MenuItem("この自治体の住民一覧を見る",this::listResidents,true,Material.BOOK,data));
+        menus.add(new MenuItem("前ページに戻る",this::executeGui,true,Material.ARROW,null,8,0));
+        Gui.getInstance().openMenu(menuItem.getClicker(),"自治体:"+data.get(1),menus);
+    }
+
+    private void addResidents(MenuItem menuItem) {
+        String data = (String) menuItem.getCustomData();
+        sqlUtil.MunicipalData municipalData = sqlUtil.getMunicipal(data);
+        List<String> member = Objects.requireNonNull(municipalData).member();
+        member.add(menuItem.getClicker().getUniqueId().toString());
+        sqlUtil.addMunicipal(municipalData.uuid(),data,member);
+        ChatUtil.success(menuItem.getClicker(),"正常に自治体の住民になりました！");
+    }
+
+    private void remove(MenuItem menuItem) {
+        List<?> data = (List<?>) menuItem.getCustomData();
+        final DynmapAPI dynmap = MetaversePlugin.getDynmap();
+        final MarkerAPI marker = dynmap.getMarkerAPI();
+        MarkerSet markerSet = marker.getMarkerSet("municipal");
+        if (markerSet == null) {
+            markerSet = marker.createMarkerSet("municipal", "自治体", null, true);
+        }
+        markerSet.findAreaMarker((String) data.get(0)).deleteMarker();
+        sqlUtil.removeMunicipal((String) data.get(0));
+    }
+
+    private void listResidents(MenuItem menuItem) {
+        List<?> data = (List<?>) menuItem.getCustomData();
+        sqlUtil.MunicipalData municipalData = Objects.requireNonNull(sqlUtil.getMunicipal((String) data.get(0)));
+        Gui.getInstance().openMultiPageMenu(menuItem.getClicker(), "自治体の住民一覧", municipalData.member().stream().map(n -> {
+            ItemStack item = new ItemStack(Material.PLAYER_HEAD);
+            SkullMeta skull = (SkullMeta) item.getItemMeta();
+            skull.setOwningPlayer(Bukkit.getOfflinePlayer(municipalData.uuid()));
+            item.setItemMeta(skull);
+            return new MenuItem(Bukkit.getOfflinePlayer(municipalData.uuid()).getName(),null,false,item);
+        }).collect(Collectors.toList()),new MenuItem("前ページに戻る",this::manage,true,Material.ARROW,data));
     }
 
     /**
