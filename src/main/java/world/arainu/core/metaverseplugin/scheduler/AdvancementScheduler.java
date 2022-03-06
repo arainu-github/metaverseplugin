53c700278b07339a9285baf3c90d75260f841a86
kumitatepazuru <riku.ueda@outlook.jp>
2022-03-06 18:43:09 +0900

    軽量化

--- src/main/java/world/arainu/core/metaverseplugin/scheduler/AdvancementScheduler.java
+++ src/main/java/world/arainu/core/metaverseplugin/scheduler/AdvancementScheduler.java
@@ -8,6 +8,7 @@ import world.arainu.core.metaverseplugin.listener.AdvancementListener;
 import world.arainu.core.metaverseplugin.utils.sqlUtil;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static world.arainu.core.metaverseplugin.listener.AdvancementListener.addPlayerAdvancement;
 import static world.arainu.core.metaverseplugin.listener.AdvancementListener.removeQueue;
@@ -16,15 +17,21 @@ public class AdvancementScheduler extends BukkitRunnable {
     @Override
     public void run() {
         List<Player> queue = AdvancementListener.getSyncQueue();
-        if (queue.size() > 0) {
+        if(queue.size() > 0) {
             Player p = queue.get(0);
             if (p.isOnline()) {
-                Bukkit.getScheduler().runTaskAsynchronously(MetaversePlugin.getInstance(), () -> {
-                    MetaversePlugin.logger().info("syncing advancement data... size:" + queue.size());
-                    sqlUtil.removePlayerAdvancement(p.getUniqueId());
-                    Bukkit.advancementIterator().forEachRemaining(advancement -> addPlayerAdvancement(advancement, p));
-                    MetaversePlugin.logger().info("synced");
+                MetaversePlugin.logger().info("syncing advancement data... size:" + queue.size());
+                sqlUtil.removePlayerAdvancement(p.getUniqueId());
+                AtomicInteger i = new AtomicInteger();
+                AtomicInteger j = new AtomicInteger();
+                Bukkit.advancementIterator().forEachRemaining(advancement -> {
+                    Bukkit.getScheduler().runTaskLater(MetaversePlugin.getInstance(),() -> addPlayerAdvancement(advancement, p),j.get());
+                    i.getAndIncrement();
+                    if(j.get() == 50){
+                        j.getAndIncrement();
+                    }
                 });
+                MetaversePlugin.logger().info("synced");
             }
             removeQueue();
         }
